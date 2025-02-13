e1bc6ba1d8c4e2d478fef947835970b7b592be06
MohamedMesto <mohamedmesto111@gmail.com>
2025-02-13 21:05:49 +0100

    Added products views and templates

--- ec_products/urls.py
+++ ec_products/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='ec_home')
+    path('', views.all_ec_products, name='ec_products')
 ]
-
-
--- ec_products/views.py
+++ ec_products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import EC_Product
 
 # Create your views here.
+
+def all_ec_products(request):
+    """ A view to show all ec products, including sorting and search queries """
+
+    ec_products = EC_Product.objects.all()
+
+    context = {
+        'ec_products': ec_products,
+    }
+
+    return render(request, 'ec_products/ec_products.html', context)
