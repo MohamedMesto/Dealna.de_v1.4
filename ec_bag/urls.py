20069c1a8b2fed073d6a5151df116f181fadbe0c
MohamedMesto <mohamedmesto111@gmail.com>
2025-02-15 15:50:36 +0100

    Add adjust bag and remove from bag views, and calc subtotal template filter

--- ec_bag/urls.py
+++ ec_bag/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_ec_bag, name='view_ec_bag'),
     path('add/<item_id>/', views.add_to_ec_bag, name='add_to_ec_bag'),
+    path('adjust/<item_id>/', views.adjust_ec_bag, name='adjust_ec_bag'),
+    path('remove/<item_id>/', views.remove_from_ec_bag, name='remove_from_ec_bag'),
 ]
--- ec_bag/views.py
+++ ec_bag/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import redirect, render
+ 
+
+from django.shortcuts import render, redirect, HttpResponse,reverse
+ 
+
+# from django.urls import reverse
 
 # Create your views here.
 
@@ -32,8 +37,51 @@ def add_to_ec_bag(request, item_id):
         else:
             ec_bag[item_id] = quantity
 
-
     request.session['ec_bag'] = ec_bag
     # print(request.session['ec_bag'])
     return redirect(redirect_url)
+
+
+
+def adjust_ec_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+    
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'ec_product_size' in request.POST:
+        size = request.POST['ec_product_size']
+    ec_bag = request.session.get('ec_bag', {})
+    if size:
+        if quantity > 0:
+            ec_bag[item_id]['items_by_size'][size] = quantity
+        else:
+            del ec_bag[item_id]['items_by_size'][size]
+            if not ec_bag[item_id]['items_by_size']:
+                ec_bag.pop(item_id)
+    else:
+        if quantity > 0:
+            ec_bag[item_id] = quantity
+        else:
+            ec_bag.pop(item_id)
+    request.session['ec_bag'] = ec_bag
+    return redirect(reverse('view_ec_bag'))
+
+def remove_from_ec_bag(request, item_id):
+    """Remove the item from the shopping ec_bag"""
+    try:
+        size = None
+        if 'ec_product_size' in request.POST:
+            size = request.POST['ec_product_size']
+        ec_bag = request.session.get('ec_bag', {})
+        if size:
+            del ec_bag[item_id]['items_by_size'][size]
+            if not ec_bag[item_id]['items_by_size']:
+                ec_bag.pop(item_id)
+        else:
+            ec_bag.pop(item_id)
+        request.session['ec_bag'] = ec_bag
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
+ 
     
