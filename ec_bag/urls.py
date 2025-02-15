eebf8ec8e39c2ef467e66bd9fdaa48465fd9a225
MohamedMesto <mohamedmesto111@gmail.com>
2025-02-15 11:25:20 +0100

    Add add to ec_bag functionality

--- ec_bag/urls.py
+++ ec_bag/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_ec_bag, name='view_ec_bag')
+    path('', views.view_ec_bag, name='view_ec_bag'),
+    path('add/<item_id>/', views.add_to_ec_bag, name='add_to_ec_bag'),
 ]
--- ec_bag/views.py
+++ ec_bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 
@@ -6,3 +6,21 @@ def view_ec_bag(request):
     """ A view to renders the ec_bag contents page """
 
     return render(request, 'ec_bag/ec_bag.html')
+
+
+def add_to_ec_bag(request, item_id):
+    """ Add a quantity of the specified ec_product to the shopping ec_bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    ec_bag = request.session.get('ec_bag', {})
+
+    if item_id in list(ec_bag.keys()):
+        ec_bag[item_id] += quantity
+    else:
+        ec_bag[item_id] = quantity
+
+    request.session['ec_bag'] = ec_bag
+    print(request.session['ec_bag'])
+    return redirect(redirect_url)
+    
