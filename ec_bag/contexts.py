c8c0f8d9bdd8bc5aa01da2aad09c6b3294b6091f
MohamedMesto <mohamedmesto111@gmail.com>
2025-02-15 13:43:04 +0100

    Finished product size logic

--- ec_bag/contexts.py
+++ ec_bag/contexts.py
@@ -10,15 +10,27 @@ def ec_bag_contents(request):
     ec_product_count = 0
     ec_bag = request.session.get('ec_bag', {})
 
-    for item_id, quantity in ec_bag.items():
-        ec_product = get_object_or_404(EC_Product, pk=item_id)
-        total += quantity * ec_product.price
-        ec_product_count += quantity
-        ec_bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'ec_product': ec_product,
-        })
+    for item_id, item_data in ec_bag.items():
+        if isinstance(item_data, int):
+            ec_product = get_object_or_404(EC_Product, pk=item_id)
+            total += item_data * ec_product.price
+            ec_product_count += item_data
+            ec_bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'ec_product': ec_product,
+            })
+        else:
+            ec_product = get_object_or_404(EC_Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * ec_product.price
+                ec_product_count += quantity
+                ec_bag_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'ec_product': ec_product,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
--- ec_bag/views.py
+++ ec_bag/views.py
@@ -13,12 +13,25 @@ def add_to_ec_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'ec_product_size' in request.POST:
+        size = request.POST['ec_product_size']
     ec_bag = request.session.get('ec_bag', {})
 
-    if item_id in list(ec_bag.keys()):
-        ec_bag[item_id] += quantity
+    if size:
+        if item_id in list(ec_bag.keys()):
+            if size in ec_bag[item_id]['items_by_size'].keys():
+                ec_bag[item_id]['items_by_size'][size] += quantity
+            else:
+                ec_bag[item_id]['items_by_size'][size] = quantity
+        else:
+            ec_bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        ec_bag[item_id] = quantity
+        if item_id in list(ec_bag.keys()):
+            ec_bag[item_id] += quantity
+        else:
+            ec_bag[item_id] = quantity
+
 
     request.session['ec_bag'] = ec_bag
     # print(request.session['ec_bag'])
