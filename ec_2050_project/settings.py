4f51ef2c16ab4458210938ccf28748f4c32d8c7f
MohamedMesto <mohamedmesto111@gmail.com>
2025-02-16 23:09:56 +0100

    Added class methods to webhook handler and webhook view

--- ec_2050_project/settings.py
+++ ec_2050_project/settings.py
@@ -35,7 +35,7 @@ STRIPE_CURRENCY = 'usd'
 #STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY")
 STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
-STRIPE_SECRET_KEY = os.environ.get("STRIPE_WH_SECRET")
+STRIPE_WH_SECRET = os.environ.get("STRIPE_WH_SECRET")
 
 #STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 #STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
@@ -44,7 +44,10 @@ STRIPE_SECRET_KEY = os.environ.get("STRIPE_WH_SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 
+                 'localhost', # listen for stripe webhooks
+                 '.herokuapp.com'
+                 ]
 
 
 # Application definition
