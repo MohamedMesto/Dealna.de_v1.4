1679c15e38c073f6269409523d7f59f30f78e4cb
MohamedMesto <mohamedmesto111@gmail.com>
2025-02-19 23:53:08 +0100

    Added profile form, views and updated template

--- ec_profiles/views.py
+++ ec_profiles/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+
 
 from .models import EC_UserProfile
+from .forms import EC_UserProfileForm
 
 def ec_profile(request):
-    """ Display the user's profile. """
+    """ Display the user's ec_profile. """
     ec_profile = get_object_or_404(EC_UserProfile, user=request.user)
+    if request.method == 'POST':
+        form = EC_UserProfileForm(request.POST, instance=ec_profile)
+        if form.is_valid():
+                form.save()
+        messages.success(request, 'Profile updated successfully')
+
+    form = EC_UserProfileForm(instance=ec_profile)
+    ec_orders = ec_profile.ec_orders.all()
+
+
     template = 'ec_profiles/ec_profile.html'
     context = {
-    'ec_profile': ec_profile,
+        'form': form,
+        'ec_orders': ec_orders,
+        'on_profile_page': True
     }
     return render(request, template, context)
