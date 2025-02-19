5637384e04f24ac82b61f2f7e97b6a2c0c7957fc
MohamedMesto <mohamedmesto111@gmail.com>
2025-02-19 23:28:18 +0100

    Updated allauth templates and tested profiles

--- ec_profiles/models.py
+++ ec_profiles/models.py
@@ -27,4 +27,4 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         EC_UserProfile.objects.create(user=instance)
     # Existing users: just save the profile
-    instance.ec_userprofile.save()
+    instance.ec_userprofile.save()  
--- ec_profiles/views.py
+++ ec_profiles/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import EC_UserProfile
+
 def ec_profile(request):
     """ Display the user's profile. """
+    ec_profile = get_object_or_404(EC_UserProfile, user=request.user)
     template = 'ec_profiles/ec_profile.html'
-    context = {}
+    context = {
+    'ec_profile': ec_profile,
+    }
     return render(request, template, context)
