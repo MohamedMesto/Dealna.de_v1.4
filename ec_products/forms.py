6415c24d1b8a7fcf0ed86b47c1fb581170868f3b
MohamedMesto <mohamedmesto111@gmail.com>
2025-02-20 23:09:36 +0100

    Product Admin - Product Form

---
+++ ec_products/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import EC_Product, EC_Category
+
+
+class EC_ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = EC_Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        ec_categories = EC_Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in ec_categories]
+
+        self.fields['ec_category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
