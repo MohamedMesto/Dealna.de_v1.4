7b8bbfe1dd8fa8a1b3e780585186049d051592f9
MohamedMesto <mohamedmesto111@gmail.com>
2025-02-22 15:16:16 +0100

    set Debug 2

--- ec_2050_project/settings.py
+++ ec_2050_project/settings.py
@@ -47,19 +47,25 @@ DEFAULT_FROM_EMAIL = 'EC_platform_2050@Loqta2050.com'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else: 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+# if 'DATABASE_URL' in os.environ:
+#     DATABASES = {
+#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#     }
+# else: 
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
-
+ 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
