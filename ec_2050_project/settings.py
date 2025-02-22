8e94c7aeb459280b7c67383ac3463fc7be3bfc02
MohamedMesto <mohamedmesto111@gmail.com>
2025-02-22 21:26:59 +0100

    062 base.css and login/reg refactor

--- ec_2050_project/settings.py
+++ ec_2050_project/settings.py
@@ -246,7 +246,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'EC_platform_2050@Loqta2050.com'
+    DEFAULT_FROM_EMAIL = 'loqta2050@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
@@ -255,3 +255,4 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
