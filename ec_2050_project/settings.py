5240ad2dad1116cfaf677405f3a5e74160a2e2c5
MohamedMesto <mohamedmesto111@gmail.com>
2025-02-22 19:36:51 +0100

    Update AWS Keys

--- ec_2050_project/settings.py
+++ ec_2050_project/settings.py
@@ -200,8 +200,8 @@ USE_TZ = True
 
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
+## Static files (CSS, JavaScript, Images)
+## https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
