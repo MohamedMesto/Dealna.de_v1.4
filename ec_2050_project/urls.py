4241cf6b71d44c4b9b986d76831ee70b3382eac3
MohamedMesto <mohamedmesto111@gmail.com>
2025-02-23 03:25:26 +0100

    added error 404 template

--- ec_2050_project/urls.py
+++ ec_2050_project/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
@@ -27,3 +29,5 @@ urlpatterns = [
     path('ec_checkout/', include('ec_checkout.urls')),
     path('ec_profile/', include('ec_profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'ec_2050_project.views.handler404'
