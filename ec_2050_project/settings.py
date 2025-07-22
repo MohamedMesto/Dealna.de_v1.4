92d84a2888026c1a96cc92d0a0727043b29eabe9
MohamedMesto <mohamedmesto111@gmail.com>
2025-07-22 16:52:05 +0200

    Add About app

--- ec_2050_project/settings.py
+++ ec_2050_project/settings.py
@@ -80,6 +80,7 @@ INSTALLED_APPS = [
     'ec_profiles',
     'review',    
     'faq',
+    'about',
 
     # Other
     'crispy_forms',
--- ec_2050_project/urls.py
+++ ec_2050_project/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('ec_checkout/', include('ec_checkout.urls')),
     path('ec_profile/', include('ec_profiles.urls')),
     path('faq/', include('faq.urls')),
+    path('about/', include('about.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
