78f7c262c8268212a95a1745cb84d4512f8d98de
MohamedMesto <mohamedmesto111@gmail.com>
2025-08-28 14:56:37 +0200

    Set STATIC_ROOT for Heroku collectstatic

--- ec_2050_project/settings.py
+++ ec_2050_project/settings.py
@@ -225,9 +225,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# MMM
-# Directory where 'collectstatic' will gather all static files
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 
@@ -261,6 +259,12 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
+    # MMM
+    # Directory where 'collectstatic' will gather all static files
+    # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT=STATIC_URL
+    STATIC_ROOT=MEDIA_URL
 
 #Stripe
 FREE_DELIVERY_THRESHOLD = 50
