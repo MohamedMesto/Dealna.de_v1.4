1a63eb9f151847ae5e1bf7e99ee6c83e39b38a92
MohamedMesto <mohamedmesto111@gmail.com>
2025-08-31 16:18:49 +0200

    fix py files part5

--- ec_bag/contexts.py
+++ ec_bag/contexts.py
@@ -3,13 +3,12 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from ec_products.models import EC_Product
 
-def ec_bag_contents(request):
 
+def ec_bag_contents(request):
     ec_bag_items = []
     total = 0
     ec_product_count = 0
     ec_bag = request.session.get('ec_bag', {})
-
     for item_id, item_data in ec_bag.items():
         if isinstance(item_data, int):
             ec_product = get_object_or_404(EC_Product, pk=item_id)
@@ -31,16 +30,13 @@ def ec_bag_contents(request):
                     'ec_product': ec_product,
                     'size': size,
                 })
-
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
-    
     grand_total = delivery + total
-    
     context = {
         'ec_bag_items': ec_bag_items,
         'total': total,
@@ -50,5 +46,4 @@ def ec_bag_contents(request):
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
-
     return context
--- ec_bag/views.py
+++ ec_bag/views.py
@@ -1,12 +1,9 @@
- 
-
 from django.shortcuts import render, redirect, HttpResponse,  get_object_or_404
 from django.contrib import messages
 from ec_products.models import EC_Product
 
 from django.urls import reverse
 
-# Create your views here.
 
 def view_ec_bag(request):
     """ A view to renders the ec_bag contents page """
@@ -29,30 +26,42 @@ def add_to_ec_bag(request, item_id):
         if item_id in list(ec_bag.keys()):
             if size in ec_bag[item_id]['items_by_size'].keys():
                 ec_bag[item_id]['items_by_size'][size] += quantity
-                messages.success(request, f'Updated size {size.upper()} {ec_product.name} quantity to {ec_bag[item_id]["items_by_size"][size]}')
+                messages.success(
+                    request,
+                    (
+                        f'Updated size {size.upper()} {ec_product.name} '
+                        f'quantity to {ec_bag[item_id]["items_by_size"][size]}'
+                    )
+                )
             else:
                 ec_bag[item_id]['items_by_size'][size] = quantity
-                messages.success(request, f'Added size {size.upper()} {ec_product.name} to your bag')
+                messages.success(
+                    request,
+                    f'Added size {size.upper()} {ec_product.name} to your bag')
         else:
             ec_bag[item_id] = {'items_by_size': {size: quantity}}
-            messages.success(request, f'Added size {size.upper()} {ec_product.name} to your bag')
+            messages.success(
+                request,
+                f'Added size {size.upper()} {ec_product.name} to your bag')
     else:
         if item_id in list(ec_bag.keys()):
             ec_bag[item_id] += quantity
-            messages.success(request, f'Updated {ec_product.name} quantity to {ec_bag[item_id]}')
+            messages.success(
+                request,
+                f'Updated {ec_product.name} quantity to {ec_bag[item_id]}')
         else:
             ec_bag[item_id] = quantity
-            messages.success(request, f'Added {ec_product.name} to your bag')
+            messages.success(
+                request,
+                f'Added {ec_product.name} to your bag')
 
     request.session['ec_bag'] = ec_bag
     # print(request.session['ec_bag'])
     return redirect(redirect_url)
 
 
-
 def adjust_ec_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
-    
     ec_product = get_object_or_404(EC_Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     size = None
@@ -62,24 +71,34 @@ def adjust_ec_bag(request, item_id):
     if size:
         if quantity > 0:
             ec_bag[item_id]['items_by_size'][size] = quantity
-            messages.success(request, f'Updated size {size.upper()} {ec_product.name} quantity to {ec_bag[item_id]["items_by_size"][size]}')
+            messages.success(
+                request,
+                (
+                    f'Updated size {size.upper()} {ec_product.name} '
+                    f'quantity to {ec_bag[item_id]["items_by_size"][size]}'
+                )
+            )
         else:
             del ec_bag[item_id]['items_by_size'][size]
             if not ec_bag[item_id]['items_by_size']:
                 ec_bag.pop(item_id)
-            messages.success(request, f'Removed size {size.upper()} {ec_product.name} from your bag')
+            messages.success(
+                request,
+                f'Removed size {size.upper()} {ec_product.name} from your bag')
     else:
         if quantity > 0:
             ec_bag[item_id] = quantity
-            messages.success(request, f'Updated {ec_product.name} quantity to {ec_bag[item_id]}')
+            messages.success(
+                request,
+                f'Updated {ec_product.name} quantity to {ec_bag[item_id]}')
         else:
             ec_bag.pop(item_id)
-            messages.success(request, f'Removed {ec_product.name} from your bag')
-
-
-
+            messages.success(
+                request,
+                f'Removed {ec_product.name} from your bag')
     request.session['ec_bag'] = ec_bag
     return redirect(reverse('view_ec_bag'))
+
 
 def remove_from_ec_bag(request, item_id):
     """Remove the item from the shopping ec_bag"""
@@ -93,16 +112,15 @@ def remove_from_ec_bag(request, item_id):
             del ec_bag[item_id]['items_by_size'][size]
             if not ec_bag[item_id]['items_by_size']:
                 ec_bag.pop(item_id)
-            messages.success(request, f'Removed size {size.upper()} {ec_product.name} from your bag')
+            messages.success(
+                request,
+                f'Removed size {size.upper()} {ec_product.name} from your bag')
         else:
             ec_bag.pop(item_id)
-            messages.success(request, f'Removed {ec_product.name} from your bag')
-
-
+            messages.success(
+                request, f'Removed {ec_product.name} from your bag')
         request.session['ec_bag'] = ec_bag
         return HttpResponse(status=200)
     except Exception as e:
         messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
- 
-    
