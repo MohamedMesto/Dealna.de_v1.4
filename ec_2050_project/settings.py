e2af551503280b5e2f0abfcf1538636aed56eff5
MohamedMesto <mohamedmesto111@gmail.com>
2026-01-22 22:35:08 +0100

    update SLL , TLS

--- ec_2050_project/settings.py
+++ ec_2050_project/settings.py
@@ -232,21 +232,26 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email
 if 'DEVELOPMENT' in os.environ:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'service@Dealna.de'
+    EMAIL_BACKEND = (
+        "django.core.mail.backends.console.EmailBackend"
+        )
+    DEFAULT_FROM_EMAIL = "service@Dealna.de"
 
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+    EMAIL_BACKEND = (
+        "django.core.mail.backends.smtp.EmailBackend"
+    )
+    EMAIL_USE_TLS = False
+    EMAIL_USE_SSL = True
+    EMAIL_PORT = 465
+    EMAIL_HOST = "smtp.gmail.com"
+    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
+    DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 
-    # DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
-    # EMAIL_TIMEOUT = 20
-    # SERVER_EMAIL = DEFAULT_FROM_EMAIL
+    # DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
+    EMAIL_TIMEOUT = 20
+    SERVER_EMAIL = DEFAULT_FROM_EMAIL
 
 
     
